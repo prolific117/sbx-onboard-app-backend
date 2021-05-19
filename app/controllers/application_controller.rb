d9e0179ba7cef3aa714b7aded9ae716485f13468
prolific117 <olu.oduro@gmail.com>
2021-05-19 10:44:30 +0100

    removed request method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,19 +8,17 @@ class ApplicationController < ActionController::Base
 
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PATCH'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    headers['Access-Control-Max-Age'] = "1728000"
   end
 
   def cors_preflight_check
     if request.method == "OPTIONS"
       headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PATCH'
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
       headers['Access-Control-Request-Method'] = '*'
       headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-      headers['Access-Control-Max-Age'] = '1728000'
       render :text => '', :content_type => 'text/plain'
     end
   end
