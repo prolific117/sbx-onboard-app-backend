054de5a22430c47227b8306f422bc0d71d30d356
prolific117 <olu.oduro@gmail.com>
2021-05-19 11:41:37 +0100

    removed before

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   protect_from_forgery
-  respond_to :json
 
   def current_user
     @authorization = request.headers["Authorization"]
