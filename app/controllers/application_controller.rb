636d3447d1db8861dec12702c5026b224d27fea3
prolific117 <olu.oduro@gmail.com>
2021-05-19 11:21:39 +0100

    fixed module name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   protect_from_forgery
-  before_filter :cors_preflight_check
-  after_filter :cors_set_access_control_headers
+  before_action :cors_preflight_check
+  after_action :cors_set_access_control_headers
 
   # For all responses in this controller, return the CORS access control headers.
 
