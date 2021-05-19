5909cd314bdc8634217d5e5472719169d1447c8b
prolific117 <olu.oduro@gmail.com>
2021-05-19 11:28:47 +0100

    removed old block

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,7 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   protect_from_forgery
-  before_action :cors_preflight_check
-  after_action :cors_set_access_control_headers
 
-  # For all responses in this controller, return the CORS access control headers.
-
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  end
-
-  def cors_preflight_check
-    if request.method == "OPTIONS"
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-      headers['Access-Control-Request-Method'] = '*'
-      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-      render :text => '', :content_type => 'text/plain'
-    end
-  end
 
   def current_user
     @authorization = request.headers["Authorization"]
